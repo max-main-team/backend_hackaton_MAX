392bf75dd27ebfa514f0f261c346c4a7195b8c82
ssokov <sokovykh.work@gmail.com>
2025-11-11 12:13:13 +0300

    feat: add frontend to AllowOrigins

--- internal/http/router.go
+++ internal/http/router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/max-main-team/backend_hackaton_MAX/internal/http/handlers"
 	"github.com/max-main-team/backend_hackaton_MAX/internal/services/auth"
 	echoSwagger "github.com/swaggo/echo-swagger"
@@ -12,6 +13,14 @@ import (
 
 func NewRouter(logger embedlog.Logger, userHandler *handlers.UserHandler, authHandler *handlers.AuthHandler, jwtService auth.JWTService, uniHandler *handlers.UniHandler) *echo.Echo {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"https://hackaton-max.vercel.app"},
+		AllowMethods:     []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowCredentials: true,
+		MaxAge:           86400,
+	}))
 
 	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
